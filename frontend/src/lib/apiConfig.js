07af64f107167e5d18d8949faef86e1314939c51
batchlcwd <batchlcwd@gmail.com>
2026-06-20 15:50:41 +0530

    backend url changed in frontend

--- frontend/src/lib/apiConfig.js
+++ frontend/src/lib/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const BACKEND_BASE_URL = 'http://localhost:4000/api/v1';
-export const BACKEND_BASE_URL = 'http://13.205.151.161/api/v1';
+export const BACKEND_BASE_URL = 'https://backend.substrng.shop/api/v1';
 
 export const axiosClient = axios.create({
   baseURL: BACKEND_BASE_URL,
